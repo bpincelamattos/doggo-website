713e2ce2b64bbbd022ffd3c898bb2a11b0fed2bf
Barbara Phillips <bpincelamattos@gmail.com>
2020-10-10 19:05:32 -0500

    adding changes from ajax2 exercises 4/5

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+fetch('https://dog.ceo/api/breeds/list')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let breeds = data.message;
+        let breedList = document.getElementById('breed-list');
+        breeds.forEach(function(currentBreed){
+            let breedElement = document.createElement('option');
+            breedElement.setAttribute('value',currentBreed);
+            breedElement.innerHTML = currentBreed;
+            breedList.appendChild(breedElement);
+        })
+        breedList.addEventListener("change", function(){
+            fetch(`https://dog.ceo/api/breed/${this.value}/images/random`)
+                .then(function(response){
+                    return response.json();
+                 })
+                .then(function(data){
+                    let dogImage = document.createElement('img');
+                    dogImage.setAttribute('src', data.message)
+                    document.body.appendChild(dogImage);
+                })
+        });
+    })
+
+/*
 let button = document.getElementById('generate');
 
 button.addEventListener("click", function(){
@@ -13,3 +40,4 @@ button.addEventListener("click", function(){
             button.innerHTML = 'Generate Doggo';
         })
 })
+*/
